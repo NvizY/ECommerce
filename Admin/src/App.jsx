c2b8082f4483c07a47fca1c396838ddb8af1d100
Nviz Yadav <nvizyadav4@gmail.com>
2025-08-06 21:46:48 +0530

    commit-six

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Admin from './Pages/Admin/Admin'
 import Login from './Components/Login/Login'
@@ -7,25 +7,23 @@ import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute'
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/admin/login" element={<Login />} />
-          <Route 
-            path="/admin/*" 
-            element={
-              <ProtectedRoute>
-                <div>
-                  <Navbar/>
-                  <Admin/>
-                </div>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path="/" element={<Navigate to="/admin/login" replace />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Routes>
+        <Route path="/admin/login" element={<Login />} />
+        <Route 
+          path="/admin/*" 
+          element={
+            <ProtectedRoute>
+              <div>
+                <Navbar/>
+                <Admin/>
+              </div>
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="/" element={<Navigate to="/admin/login" replace />} />
+      </Routes>
+    </div>
   )
 }
 
